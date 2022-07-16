aec9b8a5f9f59a3bb4c82475c0e434c5a70de647
mikey <michael.a.hazan@gmail.com>
2022-07-17 02:30:31 +0300

    Update #1

---
+++ src/main/java/com/mikeyypants/numb/item/ModItems.java
@@ -0,0 +1,23 @@
+package com.mikeyypants.numb.item;
+
+import com.mikeyypants.numb.Main;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
+
+    public static final RegistryObject<Item> VOID_CRYSTAL = ITEMS.register( "void_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+
+}
