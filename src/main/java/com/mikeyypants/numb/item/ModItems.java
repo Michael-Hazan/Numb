bf2bc88dd6a01f7f3609c243d2ce97c1c51035c2
mikey <michael.a.hazan@gmail.com>
2022-07-17 21:51:56 +0300

    Update #2

--- src/main/java/com/mikeyypants/numb/item/ModItems.java
+++ src/main/java/com/mikeyypants/numb/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.lwjgl.system.CallbackI;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -14,6 +15,13 @@ public class ModItems {
 
     public static final RegistryObject<Item> VOID_CRYSTAL = ITEMS.register( "void_crystal",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> UNIVERSE_AMETHYST = ITEMS.register("universe_amethyst",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
+
+    public static final RegistryObject<Item> RAW_ELEMENT_ZERO = ITEMS.register("raw_element_zero",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
+    public static final RegistryObject<Item> REFINED_ELEMENT_ZERO = ITEMS.register("refined_element_zero",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
